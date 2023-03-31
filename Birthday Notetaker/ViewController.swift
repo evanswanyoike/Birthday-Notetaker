c76c1171b201792c5063e06c83bd06312a1fb7fa
Evans Wanyoike <evanswanyoi@gmail.com>
2023-03-31 12:31:14 +0300

    Initial Commit

---
+++ Birthday Notetaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birthday Notetaker
+//
+//  Created by MAC PRO on 31/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
